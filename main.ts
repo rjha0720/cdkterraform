e1cd5c62882ec258b791b8476f960d4385dbcd6c
Rahul Jha <M1045789@mindtree.com>
2021-02-01 14:31:28 +0530

    Add project files.

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { Construct } from 'constructs';
+import { App, TerraformStack } from 'cdktf';
+import { AwsProvider } from './.gen/providers/aws';
+import { Instance } from './.gen/providers/aws/instance';
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+      super(scope, name);
+      new AwsProvider(this, 'aws', {
+          region: 'us-east-1'
+      });
+
+      new Instance(this, 'Hello', {
+          ami: "ami-2757f631",
+          instanceType: "t2.micro"
+      });
+
+    // define resources here
+
+  }
+}
+
+const app = new App();
+new MyStack(app, 'cdkterraform');
+app.synth();
